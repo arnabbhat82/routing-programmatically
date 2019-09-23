0920ccbf77c31882aa7d18fe6d3059a62e1d5db8
arnabbhat82 <arnabbhat@gmail.com>
2019-09-23 10:57:25 +0530

    routing-progrmmatically

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ParentComponent } from './parent/parent.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [{ path: 'parent', component: ParentComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
